310a415d24d99616491c1e198ea999f3e411ffda
Azad Shukor <47514903+azadshukor@users.noreply.github.com>
2019-03-12 13:58:04 +0800

    Add files via upload

---
+++ bmiCalculator.js
@@ -0,0 +1,25 @@
+//jshint eversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req,res){
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var result = weight /(height * height);
+
+    res.send("The result of the calculation is " + result);
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.")
+})
